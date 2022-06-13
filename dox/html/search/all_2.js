71b80b76e63faef84d56d77aaae2619fd7f36a8c
Centrix14 <artiom.zvyagin@yandex.ru>
2022-06-13 16:17:26 +0300

    update documentation

--- dox/html/search/all_2.js
+++ dox/html/search/all_2.js
@@ -4,7 +4,10 @@ var searchData=
   ['leto_2dlist_2eh',['leto-list.h',['../leto-list_8h.html',1,'']]],
   ['leto_5flist_5fadd_5fnode',['leto_list_add_node',['../leto-list_8c.html#a2d03b161685989dd5fa5b3697b423719',1,'leto-list.c']]],
   ['leto_5flist_5fdeinit_5fnode',['leto_list_deinit_node',['../leto-list_8c.html#a13e004ac20bf100622f4f56ff1879524',1,'leto-list.c']]],
+  ['leto_5flist_5fexpand_5flist',['leto_list_expand_list',['../leto-list_8c.html#a00ea54ecad33cebf63e32e45d385ed90',1,'leto-list.c']]],
+  ['leto_5flist_5fget_5flast',['leto_list_get_last',['../leto-list_8c.html#a019022171a68b5c9afe9c847e7fc7f97',1,'leto-list.c']]],
   ['leto_5flist_5finit_5fnode',['leto_list_init_node',['../leto-list_8c.html#aa3345051e0940dd4879b8195d1e3aa2c',1,'leto-list.c']]],
+  ['leto_5flist_5fset_5fdata',['leto_list_set_data',['../leto-list_8c.html#ab226137ee8386f7c825128d971b0702b',1,'leto-list.c']]],
   ['leto_5ft_5flist',['leto_t_list',['../leto-list_8h.html#a32567236cc5cd28cdb36d8c8debdc10b',1,'leto-list.h']]],
   ['list',['List',['../leto-list_8h.html#ac309c985dcb0d7ecd423f848f47577e7',1,'leto-list.h']]]
 ];
