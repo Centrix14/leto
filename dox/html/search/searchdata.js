c8705bb1fcc848997c43a2a3387b597925267706
Centrix14 <artiom.zvyagin@yandex.ru>
2022-06-17 20:02:10 +0300

    extend documentation

--- dox/html/search/searchdata.js
+++ dox/html/search/searchdata.js
@@ -5,7 +5,8 @@ var indexSectionsWithContent =
   2: "l",
   3: "l",
   4: "dn",
-  5: "l"
+  5: "l",
+  6: "l"
 };
 
 var indexSectionNames =
@@ -15,7 +16,8 @@ var indexSectionNames =
   2: "files",
   3: "functions",
   4: "variables",
-  5: "typedefs"
+  5: "typedefs",
+  6: "defines"
 };
 
 var indexSectionLabels =
@@ -25,6 +27,7 @@ var indexSectionLabels =
   2: "Files",
   3: "Functions",
   4: "Variables",
-  5: "Typedefs"
+  5: "Typedefs",
+  6: "Macros"
 };
 
