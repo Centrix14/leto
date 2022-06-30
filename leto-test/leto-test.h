02a31c31357356f4ce3604b572e42d3e50eedff5
Centrix14 <artiom.zvyagin@yandex.ru>
2022-06-30 14:02:10 +0300

    added documentation for leto-test

--- leto-test/leto-test.h
+++ leto-test/leto-test.h
@@ -1,11 +1,37 @@
+/**
+ * \file
+ * \author Centrix14
+ * \date 30.06.2022
+ * \brief This file provides basic functionality for unit-testing
+ */
+
 #ifndef __LETO_LIBRARY_TEST_SYSTEM_LETO_TEST_HEADER_FILE_INCLUDED__
 #define __LETO_LIBRARY_TEST_SYSTEM_LETO_TEST_HEADER_FILE_INCLUDED__
 
+/**
+ * \defgroup leto_test Simple unit testing system
+ * @{
+ */
+
 void leto_test(char *name, void (*func)());
+
+/**
+ * \brief The function of conducting a simple test
+ * \param[in] name test name
+ * \param[in] func test funcion
+ *
+ * \warning Remember that the test function must use assert for verification
+ *
+ * This function provides a simple interface for testing. If the test function
+ * returns control (test passed successfully) then the corresponding message is
+ * displayed on the screen
+ */
 
 void leto_test(char* name, void (*func)()) {
 	(*func)();
 	printf("*** %s test PASS\n", name);
 }
+
+/** @} */
 
 #endif
