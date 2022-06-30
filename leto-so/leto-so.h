c839375fd890d4c89dba26d00eaeca82a22eabb3
Centrix14 <artiom.zvyagin@yandex.ru>
2022-06-30 14:05:54 +0300

    added new library: leto-so

---
+++ leto-so/leto-so.h
@@ -0,0 +1,22 @@
+/**
+ * \file
+ * \author Centrix14
+ * \date 30.06.2022
+ * \brief leto-so library header file
+ */
+
+#ifndef __LETO_LIBRARY_SHARED_OBJECTS_SYSTEM_LETO_SO_HEADER_FILE_INCLUDED__
+#define __LETO_LIBRARY_SHARED_OBJECTS_SYSTEM_LETO_SO_HEADER_FILE_INCLUDED__
+
+/**
+ * \addtogroup leto_so
+ * @{
+ */
+
+void *leto_so_open_library(char *library_name);
+void *leto_so_get_symbol(void *library, char *symbol_name);
+void leto_so_close_library(void *library);
+
+/** @} */
+
+#endif
