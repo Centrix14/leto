b0d8cfd0961c6c7e6c727e9c9bdd342dc7734494
Centrix14 <artiom.zvyagin@yandex.ru>
2022-07-05 18:42:01 +0300

    common header file

---
+++ leto.h
@@ -0,0 +1,11 @@
+#ifndef __LETO_LIBRARY_COMMON_HEADER_HEADER_FILE_INCLUDED__
+#define __LETO_LIBRARY_COMMON_HEADER_HEADER_FILE_INCLUDED__
+
+#include "leto-list/leto-list.h"
+#include "leto-so/leto-so.h"
+
+// header-only libs
+#include "leto-error/leto-error.h"
+#include "leto-test/leto-test.h"
+
+#endif
