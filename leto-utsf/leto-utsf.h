90500db583c39cc48552e1b8b820a2a09fcfa93f
Centrix14 <artiom.zvyagin@yandex.ru>
2022-07-19 18:08:17 +0300

    add docs for leto-utsf container functions

--- leto-utsf/leto-utsf.h
+++ leto-utsf/leto-utsf.h
@@ -1,13 +1,30 @@
+/**
+ * \file
+ * \author Centrix14
+ * \date 19.07.2022
+ * \brief This file provides an interface to the UTSF library*/
+
 #ifndef __LETO_LIBRARY_UTSF_LIBRARY_LETO_UTSF_HEADER_FILE_INCLUDED__
 #define __LETO_LIBRARY_UTSF_LIBRARY_LETO_UTSF_HEADER_FILE_INCLUDED__
 
-typedef struct {
-	void *data;
-	unsigned int size;
+/** \addtogroup leto_utsf
+ * @{
+ */
 
-	char *name;
+/// UTSF container
+/**
+ * The structure of a UTSF container is a UTSF building block that stores a
+ * single value
+ */
+
+typedef struct {
+	void *data; ///< Pointer to the data stored by the container
+	unsigned int size; ///< The amount of data stored by the container
+
+	char *name; ///< Container name
 } leto_utsf_container;
 
+/// Alias for external usage
 typedef leto_utsf_container utsf_contaiter;
 
 leto_utsf_container *leto_utsf_container_init(char *name);
@@ -21,5 +38,7 @@ void leto_utsf_container_add_data(leto_utsf_container *container,
 
 leto_utsf_container *leto_utsf_container_clone(leto_utsf_container
 											  *container);
+
+/** @} */
 
 #endif
