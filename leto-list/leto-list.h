7174bf3607aeabe24ce9b71c352dfa3f132e9fac
Centrix14 <artiom.zvyagin@yandex.ru>
2022-08-16 14:00:22 +0300

    type correction

--- leto-list/leto-list.h
+++ leto-list/leto-list.h
@@ -28,19 +28,19 @@
  * taken as an argument by the iterator.
  */
 
-#define LETO_T_IFUNC_DATA(name) void (*name)(void*)
+#define LETO_T_IFUNC_DATA(name) void (*name)(common)
 
 /// Structure of a singly linked list element
 /**
- * This structure implements an element of a singly linked list. These lists can
- * be used not only inside the library, but also outside it. Be careful when
- * working with this structure, remember that it stores a general type pointer
- * to your data.
+ * This structure implements an element of a singly linked list. These lists
+ * can be used not only inside the library, but also outside it. Be careful
+ * when working with this structure, remember that it stores a general type
+ * pointer to your data.
  */
 
 struct __leto_list_list_t__ {
 	struct __leto_list_list_t__ *next; ///< Pointer to the next element
-	void *data; ///< Pointer to the data stored by the element
+	common data; ///< Pointer to the data stored by the element
 };
 
 /// typedef for __leto_list_list_t__
@@ -53,25 +53,26 @@ typedef struct __leto_list_list_t__ leto_t_list;
 
 /// typdef for __leto_list_list_t__
 /**
- * List (abbreviation for __leto_list_list_t__) is intended for use outside the
- * library, can be replaced by leto_t_list.
+ * List (abbreviation for __leto_list_list_t__) is intended for use outside
+ * the library, can be replaced by leto_t_list.
  */
 
 typedef struct __leto_list_list_t__ List;
 
-leto_t_list *leto_list_init_node(leto_t_list *next, void *data);
+leto_t_list *leto_list_init_node(leto_t_list *next, common data);
 void leto_list_deinit_node(leto_t_list *deinitialized_node);
-leto_t_list *leto_list_add_node(leto_t_list *parent_node, void *data);
+leto_t_list *leto_list_add_node(leto_t_list *parent_node, common data);
 leto_t_list *leto_list_get_last(leto_t_list *list);
-leto_t_list *leto_list_expand_list(leto_t_list *list, void *data);
-void leto_list_set_data(leto_t_list *node, void *data);
+leto_t_list *leto_list_expand_list(leto_t_list *list, common data);
+void leto_list_set_data(leto_t_list *node, common data);
 void leto_list_iterator(leto_t_list *list,
 						LETO_T_IFUNC_NODE(node_func),
 						LETO_T_IFUNC_DATA(data_func));
 void leto_list_foreach(leto_t_list *list, LETO_T_IFUNC_DATA(data_func));
-leto_t_list *leto_list_get_by_index(leto_t_list *list, unsigned int pos);
-void leto_list_insert_node(leto_t_list *list, leto_t_list *node, unsigned int pos);
-leto_t_list *leto_list_eject_node(leto_t_list *list, unsigned int pos);
+leto_t_list *leto_list_get_by_index(leto_t_list *list, position pos);
+void leto_list_insert_node(leto_t_list *list, leto_t_list *node,
+						   position pos);
+leto_t_list *leto_list_eject_node(leto_t_list *list, position pos);
 
 /** @} */
 
