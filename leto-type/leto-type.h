89525169b7954ceff81a9e6a32b76501d2f61974
Centrix14 <artiom.zvyagin@yandex.ru>
2022-08-16 14:26:23 +0300

    miniature type library

---
+++ leto-type/leto-type.h
@@ -0,0 +1,27 @@
+/**
+ * \file
+ * \author Centrix14
+ * \date 16.08.2022
+ * \brief Miniature Type Library
+ */
+
+#ifndef __LETO_LIBRARY_TYPE_LIBRARY_LETO_TYPE_HEADER_FILE_INCLUDED__
+#define __LETO_LIBRARY_TYPE_LIBRARY_LETO_TYPE_HEADER_FILE_INCLUDED__
+
+/**
+ * \defgroup leto_type [leto-type] Miniature type library
+ * @{
+ */
+
+/// Type for a general purpose pointer
+typedef void* common;
+
+/// Type for positive numbers
+typedef unsigned int position;
+
+/// Position type
+typedef unsigned int positive;
+
+/** @} */
+
+#endif
