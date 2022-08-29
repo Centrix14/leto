14430af2497b0e75ac993a9726379e2e0b8b6b72
Centrix14 <artiom.zvyagin@yandex.ru>
2022-08-29 13:25:35 +0300

    add new includes

--- leto.h
+++ leto.h
@@ -1,11 +1,19 @@
+/**
+ * \file
+ * \author Centrix14
+ * \date 29.08.2022
+ * \brief General header for the library
+ */
+
 #ifndef __LETO_LIBRARY_COMMON_HEADER_HEADER_FILE_INCLUDED__
 #define __LETO_LIBRARY_COMMON_HEADER_HEADER_FILE_INCLUDED__
 
-#include "leto-list/leto-list.h"
-#include "leto-so/leto-so.h"
-
-// header-only libs
+#include "leto-type/leto-type.h"
 #include "leto-error/leto-error.h"
 #include "leto-test/leto-test.h"
+
+#include "leto-list/leto-list.h"
+#include "leto-so/leto-so.h"
+#include "leto-utsf/leto-utsf.h"
 
 #endif
