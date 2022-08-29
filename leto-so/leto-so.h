a80779c1ed12f431b34ba353816087a8f624f5a8
Centrix14 <artiom.zvyagin@yandex.ru>
2022-08-29 13:23:32 +0300

    move here all includes for leto-so

--- leto-so/leto-so.h
+++ leto-so/leto-so.h
@@ -8,6 +8,9 @@
 #ifndef __LETO_LIBRARY_SHARED_OBJECTS_SYSTEM_LETO_SO_HEADER_FILE_INCLUDED__
 #define __LETO_LIBRARY_SHARED_OBJECTS_SYSTEM_LETO_SO_HEADER_FILE_INCLUDED__
 
+#include "../leto-type/leto-type.h"
+#include "../leto-error/leto-error.h"
+
 /**
  * \addtogroup leto_so
  * @{
