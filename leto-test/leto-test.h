53ff5f3a3f6b61ce6c46464bec1f5ad8aa7119ab
Centrix14 <artiom.zvyagin@yandex.ru>
2022-09-13 19:43:04 +0300

    remove realization from header file: it causes conflicts in OpenPencil

--- leto-test/leto-test.h
+++ leto-test/leto-test.h
@@ -8,29 +8,14 @@
 #ifndef __LETO_LIBRARY_TEST_SYSTEM_LETO_TEST_HEADER_FILE_INCLUDED__
 #define __LETO_LIBRARY_TEST_SYSTEM_LETO_TEST_HEADER_FILE_INCLUDED__
 
+#include <stdio.h>
+
 /**
  * \defgroup leto_test [leto-test] Simple unit testing system
  * @{
  */
 
 void leto_test(char *name, void (*func)());
-
-/**
- * \brief The function of conducting a simple test
- * \param[in] name test name
- * \param[in] func test funcion
- *
- * \warning Remember that the test function must use assert for verification
- *
- * This function provides a simple interface for testing. If the test function
- * returns control (test passed successfully) then the corresponding message is
- * displayed on the screen
- */
-
-void leto_test(char* name, void (*func)()) {
-	(*func)();
-	printf("*** %s test PASS\n", name);
-}
 
 /** @} */
 
